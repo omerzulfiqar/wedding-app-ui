2b33a816de40b1d5e0311f3e06103445edaec347
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-12 23:19:06 -0400

    calendar download added to events

--- src/pages/EventsInfo.js
+++ src/pages/EventsInfo.js
@@ -4,53 +4,39 @@ import React, { Component } from 'react';
 import { Container, Typography, Stack, Card, CardContent, Link } from '@mui/material';
 import axios from 'axios';
 import { LOCAL_API_URL } from '../config';
+import AddToCalendar from '@culturehq/add-to-calendar';
+import '../App.css';
 
 export default class EventsInfo extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      allowedEvents: [
-        {
-          name: 'Mehndi',
-          eventId: 0,
-          venue: 'Cherry Blossom Banquet Hall',
-          address: '46110 Lake Center Plaza, Sterling, VA 20165',
-          mapsLink: 'https://goo.gl/maps/MVdQSYb4nuX2bFSY8',
-          timeOfEvent: 'December 23rd, 2021 at 6:00 PM',
-        },
-        {
-          name: 'Nikkah',
-          eventId: 1,
-          venue: 'Tanveer Residence',
-          address: '6231 Lawson Drive, Haymarket, VA 20169',
-          mapsLink: 'https://goo.gl/maps/uNbai5Rmo1Ds2pK1A',
-          timeOfEvent: 'December 24th, 2021 at 3:00 PM',
-        },
-        {
-          eventId: 2,
-          venue: 'Hilton Washington Dulles',
-          address: '13869 Park Center Rd, Herndon, VA 20171',
-          mapsLink: 'https://goo.gl/maps/aepo2fcdLhURLrUW9',
-          name: 'Reception',
-          ballroom: 'Belmont Ballroom',
-          timeOfEvent: 'December 26th, 2021 at 6:00 PM',
-        },
-      ],
+      allowedEvents: null,
     };
   }
 
-  // componentDidMount = async () => {
-  //   const { guestCode } = this.props.match.params;
-  //   try {
-  //     const response = await axios.get(`${LOCAL_API_URL}/eventsInformation/${guestCode}`);
-  //     const { events } = response.data;
-  //     this.setState({ allowedEvents: events });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  componentDidMount = async () => {
+    const { guestCode } = this.props.match.params;
+    try {
+      const response = await axios.get(`${LOCAL_API_URL}/eventsInformation/${guestCode}`);
+      const { events } = response.data;
+      this.setState({ allowedEvents: events });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  renderCalendarEvent = (event) => {
+    const calendarEvent = {
+      name: `Omer's & Kayanat's ${event.name}`,
+      details: `At ${event.address}. ${event.mapsLink}`,
+      location: `${event.mapsLink}`,
+      startsAt: `${event.startsAt}`,
+      endsAt: `${event.endsAt}`,
+    };
+    return <AddToCalendar event={calendarEvent} filename={`Omer's & Kayanat's ${event.name}`} />;
+  };
   render() {
     const { allowedEvents } = this.state;
     return (
@@ -66,30 +52,29 @@ export default class EventsInfo extends Component {
               else if (event.name === 'Nikkah') name = 'Nikkah Ceremony & Reception';
               else if (event.name === 'Reception') name = 'Wedding Reception';
               return (
-                <Card
-                  variant="outlined"
-                  key={event.name}
-                  style={{ marginTop: 30, marginBottom: 30 }}>
-                  <CardContent>
-                    <Typography variant="h5">{name}</Typography>
-                    <div id="location" style={{ display: 'inline-flex' }}>
-                      <Typography variant="body1">
-                        Venue:{' '}
-                        <Link href={event.mapsLink}>
-                          {event.venue}
-                          {event.ballroom && `, ${event.ballroom}`}
-                        </Link>{' '}
-                      </Typography>
-                    </div>
-                    <div id="address">
-                      <Typography variant="body1">Address: {event.address}</Typography>
-                    </div>
-                    <div id="time">
-                      <Typography variant="body1">Time: {event.timeOfEvent}</Typography>
-                    </div>
-                    {this.renderCalendarEvent(event)}
-                  </CardContent>
-                </Card>
+                <div key={event.name} style={{ marginTop: 30, marginBottom: 30 }}>
+                  <Card variant="outlined" key={event.name}>
+                    <CardContent>
+                      <Typography variant="h5">{name}</Typography>
+                      <div id="location" style={{ display: 'inline-flex' }}>
+                        <Typography variant="body1">
+                          Venue:{' '}
+                          <Link href={event.mapsLink}>
+                            {event.venue}
+                            {event.ballroom && `, ${event.ballroom}`}
+                          </Link>{' '}
+                        </Typography>
+                      </div>
+                      <div id="address">
+                        <Typography variant="body1">Address: {event.address}</Typography>
+                      </div>
+                      <div id="time">
+                        <Typography variant="body1">Time: {event.timeOfEvent}</Typography>
+                      </div>
+                    </CardContent>
+                  </Card>
+                  {this.renderCalendarEvent(event)}
+                </div>
               );
             })}
           </Stack>
