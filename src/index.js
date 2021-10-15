ccb587ad4f364a580da191817f3587b550c9d618
Omer Zulfiqar <46098753+omerzulfiqar@users.noreply.github.com>
2021-10-15 16:40:10 -0400

    Revert "pwa done"

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createTheme, ThemeProvider } from '@mui/material';
-import * as serviceWorker from './serviceWorker';
 
 const theme = createTheme({
   palette: {
@@ -59,11 +58,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
