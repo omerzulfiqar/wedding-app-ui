4754dab820a63d7b567ef40a47954e2e003fb53b
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-18 01:49:20 -0400

    icon and name changed

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import { Circles, Rings } from '@agney/react-loading';
+import { Circles, ThreeDots } from '@agney/react-loading';
 import { Container } from '@mui/material';
 
 export default class Loading extends Component {
@@ -8,7 +8,7 @@ export default class Loading extends Component {
     return (
       <Container maxWidth="xs" style={{ textAlign: 'center', marginTop: 30 }}>
         {this.props.page && <Circles width="20%" style={{ color: '#1D2C4C' }} />}
-        {this.props.form && <Rings width="20%" style={{ color: '#1D2C4C' }} />}
+        {this.props.form && <ThreeDots width="20%" style={{ color: '#1D2C4C' }} />}
       </Container>
     );
   }
